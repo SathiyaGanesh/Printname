9aa33171954df6757b70116d481ee9870282ed74
SathiyaGanesh <sathiyapriya.r23@gmail.com>
2025-06-01 18:25:19 +0530

    coded

---
+++ src/org/sampleprint/PrintNames.java
@@ -0,0 +1,12 @@
+package org.sampleprint;
+
+public class PrintNames {
+public static void main(String[] args) {
+	String name="Sathiya";
+	int id=42;
+	String dept="EEE";
+System.out.println(name);	
+System.out.println(id);
+System.out.println(dept);
+}
+}
