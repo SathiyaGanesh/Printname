7aeabeb4862d906c97f56b384d251c60340b8d52
SathiyaGanesh <sathiyapriya.r23@gmail.com>
2025-06-01 18:35:49 +0530

    code changed

--- src/org/sampleprint/PrintNames.java
+++ src/org/sampleprint/PrintNames.java
@@ -5,8 +5,10 @@ public static void main(String[] args) {
 	String name="Sathiya";
 	int id=42;
 	String dept="EEE";
+	String add="Chennai";
 System.out.println(name);	
 System.out.println(id);
 System.out.println(dept);
+System.out.println(add);
 }
 }
